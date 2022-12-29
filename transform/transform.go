800566a3d87557646d236f1c1e75b5eb40bde258
Pav0l <pavol.karas@gmail.com>
2022-12-29 10:11:14 +0100

    validate indexes of new header columns before categorizing them

--- transform/transform.go
+++ transform/transform.go
@@ -9,10 +9,24 @@ import (
 )
 
 type Transform struct {
-	supportedHeaders map[string]string
 	c                categorize.Categorize
-	hadSpenderAlrdy  bool
+	supportedHeaders map[string]string
+	// set to `true` if the original CSV has "Spender" column
+	hadSpenderAlrdy bool
+	// header row indexes we want to keep
+	originalRowIndexesToKeep []int8
+	// headerIndex stores a map of header name and its index after rebuilding the original header
+	headerIndex headerIndex
 }
+
+type headerIndex map[string]int8
+
+const SUM_HEADER = "Sum"
+const PARTNER_HEADER = "Partner"
+const OG_CATEGORY_HEADER = "Original Category"
+const SPENDER_HEADER = "Spender"
+const CATEGORY_HEADER = "Category"
+const REVIEW_MANUALLY_HEADER = "Review Manually"
 
 func NewTransformer(headers string, categories categorize.Categorize) *Transform {
 	supportedHeaders := json.ReadFile(headers)
@@ -20,6 +34,7 @@ func NewTransformer(headers string, categories categorize.Categorize) *Transform
 	return &Transform{
 		supportedHeaders: supportedHeaders,
 		c:                categories,
+		headerIndex:      headerIndex{},
 		hadSpenderAlrdy:  false,
 	}
 }
@@ -27,12 +42,12 @@ func NewTransformer(headers string, categories categorize.Categorize) *Transform
 func (t *Transform) RemoveUnnecessaryColumns(rows [][]string) [][]string {
 	headers := rows[0]
 
-	newHeaders, rowIndexesToKeep := t.rebuildHeaders(headers)
+	newHeaders := t.rebuildHeaders(headers)
 	rows[0] = newHeaders
 
 	// start from index 1 to skip header row
 	for i := 1; i < len(rows); i++ {
-		rows[i] = t.rebuildRow(rows[i], rowIndexesToKeep)
+		rows[i] = t.rebuildRow(rows[i])
 	}
 
 	return rows
@@ -42,17 +57,14 @@ func (t *Transform) AppendToRow(row []string, isHeader bool, spender string) []s
 	if isHeader {
 		return t.appendToHeader(row)
 	}
-
 	// remove zero sum rows
-	sum := row[1]
+	sum := row[t.headerIndex[SUM_HEADER]]
 	if sum == "0,00" {
 		return []string{}
 	}
 
-	// I don't like this - it infers header indexes to be specific value which we do not validate anywhere
-	partner := row[2]
-	category := row[4]
-
+	partner := row[t.headerIndex[PARTNER_HEADER]]
+	category := row[t.headerIndex[OG_CATEGORY_HEADER]]
 	categorized := t.c.Categorize(partner, category)
 
 	if !t.hadSpenderAlrdy {
@@ -62,29 +74,28 @@ func (t *Transform) AppendToRow(row []string, isHeader bool, spender string) []s
 }
 
 func (t *Transform) appendToHeader(header []string) []string {
-	t.hadSpenderAlrdy = header[len(header)-1] == "Spender"
+	t.hadSpenderAlrdy = header[len(header)-1] == SPENDER_HEADER
 
 	if !t.hadSpenderAlrdy {
-		header = append(header, "Spender")
+		header = append(header, SPENDER_HEADER)
 	}
 
-	header = append(header, "Category", "Review Manually")
+	header = append(header, CATEGORY_HEADER, REVIEW_MANUALLY_HEADER)
 	return header
 }
 
-func (t *Transform) rebuildRow(original []string, rowIndexesToKeep []int8) []string {
+func (t *Transform) rebuildRow(original []string) []string {
 	newRow := []string{}
 
-	for i := 0; i < len(rowIndexesToKeep); i++ {
-		value := original[rowIndexesToKeep[i]]
+	for i := 0; i < len(t.originalRowIndexesToKeep); i++ {
+		value := original[t.originalRowIndexesToKeep[i]]
 		newRow = append(newRow, value)
 	}
 
 	return newRow
 }
 
-func (t *Transform) rebuildHeaders(headers []string) ([]string, []int8) {
-	rowIndexesToKeep := []int8{}
+func (t *Transform) rebuildHeaders(headers []string) []string {
 	newHeaders := []string{}
 
 	for i := 0; i < len(headers); i++ {
@@ -92,10 +103,12 @@ func (t *Transform) rebuildHeaders(headers []string) ([]string, []int8) {
 
 		if t.supportedHeaders[header] != "" {
 			newHeaders = append(newHeaders, t.supportedHeaders[header])
-			log.Printf(" %s is renamed to %s", header, t.supportedHeaders[header])
-			rowIndexesToKeep = append(rowIndexesToKeep, int8(i))
+			log.Printf("%s is renamed to %s", header, t.supportedHeaders[header])
+			t.originalRowIndexesToKeep = append(t.originalRowIndexesToKeep, int8(i))
+			// new header index is the last index (length -1) of indexes we want to keep
+			t.headerIndex[t.supportedHeaders[header]] = int8(len(t.originalRowIndexesToKeep) - 1)
 		}
 	}
 
-	return newHeaders, rowIndexesToKeep
+	return newHeaders
 }
